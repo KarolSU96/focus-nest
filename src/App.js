100abf9fd30627a0ff7a0b0d9ee2969a4231a50e
Karol S <karol96266@gmail.com>
2024-01-04 16:37:22 +0000

    Create styles dir, add navbar styles, change colors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import styles from './App.module.css';
 import  Navbar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <Navbar/>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,19 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import styles from '../styles/NavBar.module.css'
 
-function BasicExample() {
+const NavBar = () => {
   return (
-    <Navbar expand="md" className="bg-body-tertiary" fixed="top">
+    <Navbar expand="md" className={styles.NavBar} fixed="top">
       <Container>
         <Navbar.Brand href="#home">FocusNest</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link href="#home"><i className="fa-solid fa-house"></i>Home</Nav.Link>
-            <Nav.Link href="#link"><i className="fa-solid fa-user-plus"></i>Sign In</Nav.Link>
-            <Nav.Link href="#link">Sign Up</Nav.Link>
+            <Nav.Link href="#link"><i className="fa-solid fa-door-open"></i>Sign In</Nav.Link>
+            <Nav.Link href="#link"><i className="fa-solid fa-user-plus"></i>Sign Up</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -20,4 +21,4 @@ function BasicExample() {
   );
 }
 
-export default BasicExample;
+export default NavBar;
