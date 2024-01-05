84f5453473c7558f21ff3a3619a111dd6b16f62e
Karol S <karol96266@gmail.com>
2024-01-05 18:29:32 +0000

    Add routing to navbar and navbar styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import styles from './App.module.css';
-import  Navbar from './components/NavBar';
+import styles from "./App.module.css";
+import Navbar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Routes } from "react-router-dom";
 
+function Home() {
+  return <h1>Home page</h1>;
+}
+
+function SignIn() {
+  return <h1>Sign in</h1>;
+}
+
+function SignUp() {
+  return <h1>Sign Up</h1>;
+}
 function App() {
   return (
     <div className={styles.App}>
-      <Navbar/>
+      <Navbar />
+      <Container className={styles.Main}>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/signin" element={<SignIn/>} />
+          <Route path="/signup" element={<SignUp/>} />
+        </Routes>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,33 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import styles from '../styles/NavBar.module.css'
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar expand="md" className={styles.NavBar} fixed="top">
       <Container>
-        <Navbar.Brand href="#home">FocusNest</Navbar.Brand>
+        <NavLink to="/">
+        <Navbar.Brand ><span className={styles.test}>FocusNest</span></Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home"><i className="fa-solid fa-house"></i>Home</Nav.Link>
-            <Nav.Link href="#link"><i className="fa-solid fa-door-open"></i>Sign In</Nav.Link>
-            <Nav.Link href="#link"><i className="fa-solid fa-user-plus"></i>Sign Up</Nav.Link>
+            <NavLink  exact className={styles.NavLink} activeClassName={styles.Active} to="/">
+              <i className="fa-solid fa-house"></i>Home
+            </NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin">
+              <i className="fa-solid fa-door-open"></i>Sign In
+            </NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
+              <i className="fa-solid fa-user-plus"></i>Sign Up
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-}
+};
 
 export default NavBar;
