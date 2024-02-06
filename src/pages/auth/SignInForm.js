aef20cfce16feddd44661c0c0b625daacd79181d
Karol S <karol96266@gmail.com>
2024-02-06 14:17:06 +0000

    Add login for handling login in SingnInform

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -5,7 +5,6 @@ import btnStyles from "../../styles/Button.module.css";
 import {
   Form,
   Button,
-  Image,
   Col,
   Row,
   Container,
@@ -14,42 +13,72 @@ import {
 import axios from "axios";
 
 function SignInForm() {
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
+  const { username, password } = signInData;
+
+  const [errors, setErrors] = useState({});
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/login/", signInData);
+      navigate("/");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
+  const handleChange = (event) => {
+    setSignInData({
+      ...signInData,
+      [event.target.name]: event.target.value,
+    });
+  };
 
   return (
     <Row className={styles.Row}>
       <Col className="my-auto mx-auto py-2 p-md-2" md={6}>
         <Container className="text-center">
           <h1 className="text-center">sign in</h1>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="username">
               <Form.Label>Username</Form.Label>
               <Form.Control
-              type="text"
-              placeholder="Enter username"
-              name="username"
-              value={username}
-              onChange={handleChange}
+                type="text"
+                placeholder="Enter username"
+                name="username"
+                value={username}
+                onChange={handleChange}
               />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="password">
               <Form.Label>Password</Form.Label>
               <Form.Control
-              type="password"
-              placeholder="Password"
-              name="password"
-              value={password}
-              onChange={handleChange}
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
               />
             </Form.Group>
-            <Button className={btnStyles.ConfirmButton}variant="primary" type="submit">
+            <Button
+              className={btnStyles.ConfirmButton}
+              variant="primary"
+              type="submit"
+            >
               Sign in
             </Button>
           </Form>
         </Container>
         <Container className="mt-4 text-center">
           <Link to="/signin">
-            Don't have an account? {" "}
+            Don't have an account?{" "}
             <span className={styles.SignUpSpan} data-val="Sign up">
               Sign up
             </span>
