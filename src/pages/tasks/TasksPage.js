ffa4469081fbbca8c9a4284cb74589464a93e943
Karol S <karol96266@gmail.com>
2024-02-13 15:06:11 +0000

    Add TasksPage component

---
+++ src/pages/tasks/TasksPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function TasksPage() {
+  return (
+    <Row>
+        <Col>
+        <Contaienr>
+        <p>Test mobile</p>
+        <p>Test tasks</p>
+        </Contaienr>
+        </Col>
+    </Row>
+  )
+}
+
+export default TasksPage
