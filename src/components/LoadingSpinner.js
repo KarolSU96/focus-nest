4f62d53798c8b3317445bcc4ffba6ee78b4bc015
Karol S <karol96266@gmail.com>
2024-02-14 09:52:47 +0000

    Add spinner to HomeTasksPage

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -2,10 +2,10 @@ import React from 'react'
 import Spinner from "react-bootstrap/Spinner"
 import styles from "../styles/Spinner.module.css";
 
-function Spinner() {
+function LoadingSpinner() {
   return (
     <div className={styles.CustomSpinnerColor}><Spinner animation="border"></Spinner></div>
   )
 }
 
-export default Spinner
+export default LoadingSpinner
--- src/pages/tasks/HomeTasksPage.js
+++ src/pages/tasks/HomeTasksPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import LoadingSpinner from '../../components/LoadingSpinner';
 
 function HomeTasksPage() {
   const currentUser = useCurrentUser();
@@ -19,7 +20,7 @@ function HomeTasksPage() {
           <Col>
           <p>Test mobile</p>
           <p>Test tasks</p>
-          <Container className=""></Container>
+          <Container><LoadingSpinner/></Container>
           </Col>
       </Row>
       ): loggedOutContent}
