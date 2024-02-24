f7462186c39b90311a60db9c4455138f5af388ad
Karol S <karol96266@gmail.com>
2024-02-24 16:27:13 +0000

    Add collectionPage layout

--- src/pages/collections/CollectionPage.js
+++ src/pages/collections/CollectionPage.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Col, Row } from 'react-bootstrap'
+import { useParams } from 'react-router-dom';
+import { axiosReq } from '../../api/axiosDefaults';
 
-const CollectionPage = () => {
+
+function CollectionPage() {
+    const{id} = useParams();
+    const [collection, setCollection] = useState({results:[]});
+
+    useEffect(() => {
+        const handleMount = async() => {
+            try {
+                const [{data: collection}] = await Promise.all([
+                    axiosReq.get(`/task_collections/${id}`)
+                ])
+                setCollection({results: [collection]})
+                console.log(collection)
+            } catch(err) {
+                console.log(err)
+            }
+        }
+        handleMount();
+    }, [id])
+
+
   return (
-    <div>CollectionPage</div>
+    <Row className="mt-5 ">
+        <Col className="col-8 mx-auto">
+        {/* <Task {...collection.results[0]} setCollection={setCollection} showDots /> */}
+        Stuff here 
+        </Col>
+    </Row>
   )
 }
 
