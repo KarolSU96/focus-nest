1a839de8b87e0e4e17999b76e17bdf8dc15ee426
Karol S <karol96266@gmail.com>
2024-02-27 10:43:54 +0000

    Edit tasksPage in collectionspage to load list of collections

--- src/pages/collections/ColectionsPage.js
+++ src/pages/collections/ColectionsPage.js
@@ -4,22 +4,21 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import LoadingSpinner from "../../components/LoadingSpinner";
 import { useLocation } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
-import Task from "./Task";
+import Collection from "./Collection";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
 
 function CollecitonsPage({ message = "" }) {
-  const [tasks, setTasks] = useState({ results: [] });
+  const [collections, setCollections] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
   const [query, setQuery] = useState("");
 
   useEffect(() => {
-    const fetchTasks = async () => {
+    const fetchCollections = async () => {
       try {
-        const { data } = await axiosReq.get(`/tasks/?search=${query}`);
-        const incompleteTasks = data.results.filter((task) => !task.is_done);
-        setTasks(data);
+        const { data } = await axiosReq.get(`/task_collections/?search=${query}`);
+        setCollections(data);
         setHasLoaded(true);
         console.log(data);
       } catch (err) {
@@ -28,7 +27,7 @@ function CollecitonsPage({ message = "" }) {
     };
     setHasLoaded(false);
     const timer = setTimeout(()=> {
-      fetchTasks();
+      fetchCollections();
     }, 1000)
     return () => {
       clearTimeout(timer);
@@ -61,17 +60,16 @@ function CollecitonsPage({ message = "" }) {
               <>
                 {tasks.results.length ? (
                   <InfiniteScroll
-                  children={tasks.results
-                    .filter((task) => !task.is_done)  // Filter tasks where is_done is false
-                    .map((task) => (
-                      <div key={task.id} className="my-2">
-                        <Task {...task} setTasks={setTasks} showDots />
+                  children={collections.results
+                    .map((collection) => (
+                      <div key={collection.id} className="my-2">
+                        <Task {...collection} setCollections={setCollections} showDots />
                       </div>
                     ))}
                   dataLength={tasks.results.length}
                   loader={<LoadingSpinner />}
                   hasMore={!!tasks.next}
-                  next={() => fetchMoreData(tasks, setTasks)}
+                  next={() => fetchMoreData(collections, setCollections)}
                 />
 
                 ) : (
