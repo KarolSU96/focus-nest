e3c8119d37108614c19bb724a3b11900d902dc51
Karol S <karol96266@gmail.com>
2024-02-27 09:36:28 +0000

    Add task array to props, add tasks count to colelction

--- src/pages/collections/Collection.js
+++ src/pages/collections/Collection.js
@@ -14,8 +14,9 @@ const {
   title,
   created_at,
   due_date,
-  desciption,
-  tasks,
+  description,
+  tasks = [],
+  showDots,
 } = props;
 
 // Get current user from context
@@ -24,35 +25,35 @@ const is_owner = currentUser?.username === owner;
 
 const navigate = useNavigate();
 
-// const handleEdit = () => {
-//   navigate(`/task_collections/${id}/edit`);
-// };
+const handleEdit = () => {
+  navigate(`/task_collections/${id}/edit`);
+};
 
-// const handleDelete = async () => {
-//   try {
-//     await axiosRes.delete(`/task_collections/${id}`);
-//     if (location.pathname !== '/') {
-//       // Navigate to the root path
-//       history.push('/');
-//     } else {
-//       // Refresh the window
-//       window.location.reload();
-//     }
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
+const handleDelete = async () => {
+  try {
+    await axiosRes.delete(`/task_collections/${id}`);
+    if (location.pathname !== '/') {
+      // Navigate to the root path
+      history.push('/');
+    } else {
+      // Refresh the window
+      window.location.reload();
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 // Update collection state when props change
 useEffect(() => {
-  setTask((prevCollection) => ({
+  setCollection((prevCollection) => ({
     ...prevCollection,
     id,
   owner,
   title,
   created_at,
   due_date,
-  desciption,
+  description,
   tasks,
   }));
 }, [id,
@@ -60,59 +61,27 @@ useEffect(() => {
   title,
   created_at,
   due_date,
-  desciption,
+  description,
   tasks,]);
 
 // Initialize collection state with default values
 const [collection, setCollection] = useState({
   id,
   owner,
+  title,
   created_at,
-  task_name,
-  priority,
-  is_done,
   due_date,
-  notes,
+  description,
+  tasks,
 });
 
-// Handle marking task as done
-const handleDone = async () => {
-  try {
-    console.log(
-      "Before PUT Request - task_name:",
-      task.task_name,
-      "priority:",
-      task.priority
-    );
-
-    // Make a PUT request to update the task status
-    const { data } = await axiosRes.put(`task_collections/${id}`, {
-      is_done: !task.is_done,
-      task_name: task.task_name,
-      priority: task.priority,
-    });
-    // Update the local state with the updated task data
-    setTask((prevTask) => ({
-      ...prevTask,
-      is_done: !prevTask.is_done,
-    }));
-  } catch (err) {
-    console.log(err);
-  }
-};
 
 return (
   <Card className={`${styles.TaskCard}`}>
     <CardBody>
       <CardTitle className="d-flex justify-content-between">
-        <span>{task_name}</span>
+        <span>{title}</span>
         <div className="d-flex align-items-center">
-          <span>
-            Priority:
-            {priority === "low" ? "🔵Low" : ""}
-            {priority === "medium" ? "🟡Medium" : ""}
-            {priority === "high" ? "🔴High" : ""}
-          </span>
           {is_owner && showDots && (
             <DotsDropdown
               handleEdit={handleEdit}
@@ -124,27 +93,18 @@ return (
       <div className="d-flex justify-content-center text-center">
         <span className={styles.TaskSpan}>Created at: {created_at}</span>
         <span className={styles.TaskSpan}>
-          Mark as Done:{" "}
-          {is_owner && (
-            <label className={styles.TaskCheckboxLabel}>
-              <input
-                type="checkbox"
-                checked={task.is_done}
-                onChange={handleDone}
-              />
-            </label>
-          )}
+          Task Count: {tasks.length}
         </span>
         <span className={styles.TaskSpan}>Due Date: {due_date}</span>
       </div>
       <hr></hr>
       <div className="d-flex justify-content-center">
-        <h5 className="mb-auto">Notes:</h5>
+        <h5 className="mb-auto">Description:</h5>
       </div>
       <div
         className={`${styles.NotesContainer}d-flex justify-content-center pt-0`}
       >
-        {notes}
+        {description}
       </div>
     </CardBody>
   </Card>
