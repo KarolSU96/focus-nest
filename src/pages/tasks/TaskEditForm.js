8dec964112214d54506012acfa9c751f728bf9cd
Karol S <karol96266@gmail.com>
2024-03-04 18:10:01 +0000

    Fix error handling in TaskEditForm

--- src/pages/tasks/TaskEditForm.js
+++ src/pages/tasks/TaskEditForm.js
@@ -113,7 +113,7 @@ function TaskCreateForm() {
           onChange={handleChange}
           />
         </Form.Group>
-        {errors?.title?.map((message, idx) => (
+        {errors?.task_name?.map((message, idx) => (
         <Alert variant="warning" key={idx}>
           {message}
         </Alert>
@@ -133,7 +133,7 @@ function TaskCreateForm() {
             <option value="high">High</option>
           </Form.Control>
         </Form.Group>
-        {errors?.title?.map((message, idx) => (
+        {errors?.priority?.map((message, idx) => (
         <Alert variant="warning" key={idx}>
           {message}
         </Alert>
@@ -149,7 +149,7 @@ function TaskCreateForm() {
           onChange={handleChange}
           />
         </Form.Group>
-        {errors?.title?.map((message, idx) => (
+        {errors?.due_date?.map((message, idx) => (
         <Alert variant="warning" key={idx}>
           {message}
         </Alert>
@@ -173,7 +173,7 @@ function TaskCreateForm() {
             ))}
           </Form.Control>
         </Form.Group>
-        {errors?.title?.map((message, idx) => (
+        {errors?.task_collection?.map((message, idx) => (
         <Alert variant="warning" key={idx}>
           {message}
         </Alert>
@@ -191,7 +191,7 @@ function TaskCreateForm() {
           onChange={handleChange}
           />
         </Form.Group>
-        {errors?.title?.map((message, idx) => (
+        {errors?.notes?.map((message, idx) => (
         <Alert variant="warning" key={idx}>
           {message}
         </Alert>
