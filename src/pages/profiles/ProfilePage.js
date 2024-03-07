f5da2a15745831c2efd2626b03a72a7f5763884f
Karol S <karol96266@gmail.com>
2024-03-07 14:18:48 +0000

    Add fetch Profile function and JSX to display profile infos

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -1,32 +1,51 @@
 import React, { useEffect, useState } from "react";
 import Profile from "./Profile";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
-import { Col, Container, Image, Row, Spinner } from "react-bootstrap";
+import { Button, Col, Container, Image, Row, Spinner } from "react-bootstrap";
 import styles from "../../styles/Task.module.css";
+import customStyles from "../../styles/ProfilePage.module.css";
+import btnStyles from "../../styles/Button.module.css";
 import { axiosReq } from "../../api/axiosDefaults";
+import { useNavigate, useParams } from "react-router-dom";
 
 const ProfilePage = () => {
+
+
   const [hasLoaded, setHasLoaded] = useState(false);
   const currentUser = useCurrentUser();
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-  const [collections, setCollections] = useState({results:[]});
-  const [tasks, setTasks] = useState({results:[]});
-  
-  const userProfileName = <>{currentUser?.username}</>
+  const [profile, setProfile] = useState({});
+  const [collections, setCollections] = useState({ results: [] });
+  const [tasks, setTasks] = useState({ results: [] });
 
-  
+  const userProfileName = <>{currentUser?.username}</>;
+
+  const handleEdit = () => {
+    navigate(`/profile/${id}/edit`);
+  };
 
   useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const { data } = await axiosReq.get(`/profiles/${id}`);
+        setProfile(data);
+        console.log("Profile data:", data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
     const fetchCollections = async () => {
       try {
-        const { data } = await axiosReq.get('/task_collections/');
+        const { data } = await axiosReq.get("/task_collections/");
         setCollections(data);
-        console.log('Collections response:', data);
+        console.log("Collections response:", data);
       } catch (error) {
-        console.error('Error fetching collections:', error);
+        console.error("Error fetching collections:", error);
       }
-    }
+    };
 
     const fetchTasks = async () => {
       try {
@@ -38,39 +57,68 @@ const ProfilePage = () => {
       }
     };
 
+    fetchProfile();
     fetchTasks();
     fetchCollections();
     setHasLoaded(true);
   }, []);
 
+
+
   const completedTasksCount = Array.isArray(tasks.results)
-  ? tasks.results.filter((task) => task.is_done).length
-  : 0;
+    ? tasks.results.filter((task) => task.is_done).length
+    : 0;
 
-  const userProfile = (
-    <>
-      <Row className="text-center">
-        <Col> <Image rounded ></Image></Col>
-        <h4>{userProfileName}'s Profile</h4>
-        <p>Total numer tasks: {tasks.results.length}</p>
-        <p>Total number of collections: {collections.results.length}</p>
-        <p>Finished tasks: {completedTasksCount}</p>
-        <p>Current goals:</p>
-
-      </Row>
-      
-      <div><p>Test profile</p></div>
-    </>
-  )
+    const userProfile = (
+      <Col>
+        <Row className="text-center">
+          <Col>
+            <Image
+              className={customStyles.ProfileImage}
+              roundedCircle
+              src={profile.image}
+            ></Image>
+          </Col>
+        </Row>
+        <Row className="text-center">
+          <Col>
+            <h4>{userProfileName}'s Profile</h4>
+  
+            <p>
+              <span className="mx-1">Total tasks: {tasks.results.length}</span>
+              <span className="mx-1">
+                Total collections: {collections.results.length}
+              </span>
+              <span className="mx-1">Finished tasks: {completedTasksCount} </span>
+            </p>
+          </Col>
+        </Row>
+        <Row className="text-center" >
+          <Col>
+          <h5>
+            Current goals:
+          </h5>
+          <div>{profile.current_goals ? profile.current_goals : "Please add your goals"}</div>
+          </Col>
+        </Row>
+        <Row className="text-center mb-2">
+          <Col>
+            <Button
+              variant="primary"
+              className={`ms-1 mt-2 ${btnStyles.ConfirmButton}`}
+              onClick={handleEdit}
+            >
+              Edit Profile
+            </Button>
+          </Col>
+        </Row>
+      </Col>
+    );
   return (
     <Row className="justify-content-center">
       <Col className="col-md-8 col-lg-8 mt-3">
         <Container className={styles.TaskCard}>
-          {hasLoaded ? (
-            userProfile
-          ): (
-            <Spinner/>
-          )}
+          {hasLoaded ? userProfile : <Spinner />}
         </Container>
       </Col>
     </Row>
