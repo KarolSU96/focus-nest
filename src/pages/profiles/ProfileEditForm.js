4404e9c471110c3b0150198cbf0bee0290e0274b
Karol S <karol96266@gmail.com>
2024-03-08 13:04:10 +0000

    Add image upload and current goals change functionality to profile edit form

--- src/pages/profiles/ProfileEditForm.js
+++ src/pages/profiles/ProfileEditForm.js
@@ -6,17 +6,25 @@ import {
   Container,
   Row,
   Col,
+  Image,
 } from "react-bootstrap";
-import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../../contexts/CurrentUserContext";
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 import { useNavigate, useParams } from "react-router-dom";
 import styles from "../../styles/TaskCreateForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
-import { axiosReq, axiosRes } from "../../api/axiosDefaults";
+import { axiosReq } from "../../api/axiosDefaults";
 
 const ProfileEditForm = () => {
-  const currentUser = useContext(CurrentUserContext);
+  // const currentUser = useContext(CurrentUserContext);
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
   const { id } = useParams();
   const imageFile = useRef();
+  const navigate = useNavigate();
 
   const [profileData, setProfileData] = useState({
     current_goals: "",
@@ -25,7 +33,32 @@ const ProfileEditForm = () => {
 
   const { current_goals, image } = profileData;
 
-  
+  // const [errors, setErrors] = useState({});
+
+
+  useEffect(() => {
+    const handleMount = async () => {
+      console.log(currentUser?.profile_id?.toString())
+      if (currentUser?.profile_id?.toString() === id) {
+        console.log(id)
+        try {
+          const { data } = await axiosReq.get(`/profiles/${id}`);
+          const { current_goals, image } = data;
+          setProfileData({ current_goals, image });
+          console.log(data)
+        } catch (err) {
+          console.log(err);
+          navigate("/");
+          console.log("AAAAAAAAAAA ERROR EROOR")
+        }
+      } else {
+        console.log("BOOOOO IF STATEMENT IS NOT WORKING!!!!AAAAAA");
+      }
+    };
+
+    handleMount();
+  }, [currentUser, navigate, id]);
+
 
   const handleChange = (event) => {
     setProfileData({
@@ -40,24 +73,32 @@ const ProfileEditForm = () => {
 
     formData.append("current_goals", current_goals);
 
-
     if (imageFile?.current?.files[0]) {
-        formData.append("image", imageFile?.current?.files[0]);
-      }
-  
-      try {
-        const { data } = await axiosReq.put(`/profiles/${id}/`, formData);
-        setCurrentUser((currentUser) => ({
-          ...currentUser,
-          profile_image: data.image,
-        }));
-    }catch(err){
-        console.log(err)
+      formData.append("image", imageFile?.current?.files[0]);
+    }
+
+    try {
+      const { data } = await axiosReq.put(`/profiles/${id}`, formData);
+      setCurrentUser((currentUser) => ({
+        ...currentUser,
+        profile_image: data.image,
+      }));
+    } catch (err) {
+      console.log(err);
     }
   };
 
   const handleCancel = () => {
     navigate("/");
+  };
+
+  const handleFileChange = (e) => {
+    if (e.target.files.length) {
+      setProfileData({
+        ...profileData,
+        image: URL.createObjectURL(e.target.files[0]),
+      });
+    }
   };
 
   return (
@@ -81,21 +122,38 @@ const ProfileEditForm = () => {
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="profileImage">
-              <Form.Label>Change Profile Image</Form.Label>
-              <Form.File
-                
+            {image && (
+                <figure>
+                  <Image src={image} fluid />
+                </figure>
+              )}
+              <Form.Label
+              htmlFor="image-upload"
+              >
+                Change Profile Image</Form.Label>
+              {/* <Form.File
+                id="image-upload"
+                ref={imageFile}
+                accept="image/*"
+                onChange={(e) => {
+                  if (e.target.files.length) {
+                    setProfileData({
+                      ...profileData,
+                      image: URL.createObjectURL(e.target.files[0]),
+                    });
+                  }
+                }}
+              /> */}
+              <Form.Control
+              id="image-upload"
+              type="file"
+              accept="image/*"
+              ref={imageFile}
+              onChange={handleFileChange}
               />
-              <Form.Control type="file" accept="image/*" ref={imageFile} />
             </Form.Group>
 
-            {/* {image && (
-              <img
-                src={URL.createObjectURL(image)}
-                alt="Profile Preview"
-                className="img-fluid rounded-circle mb-3"
-                style={{ maxWidth: "100px", maxHeight: "100px" }}
-              />
-            )} */}
+            
 
             <Button
               variant="primary"
