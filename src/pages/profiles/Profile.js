94c877f03ab4b30637908fded1fe3ad2b480f146
Karol S <karol96266@gmail.com>
2024-03-11 17:43:09 +0000

    Add comments, format

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -5,29 +5,30 @@ import { axiosReq } from "../../api/axiosDefaults";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 const Profile = () => {
-
+  // State to track whether the data has been loaded
   const [hasLoaded, setHasLoaded] = useState(false);
+  // Get the current user from the context
   const currentUser = useCurrentUser();
 
-  const [collections, setCollections] = useState({results:[]});
-  const [tasks, setTasks] = useState({results:[]});
-  
-  const userProfileName = <>{currentUser?.username}</>
+  // State to store user collections and tasks
+  const [collections, setCollections] = useState({ results: [] });
+  const [tasks, setTasks] = useState({ results: [] });
 
-  
+  // JSX element for displaying the user's profile name
+  const userProfileName = <>{currentUser?.username}</>;
 
   useEffect(() => {
-
+    // Function to fetch user collections from the API
     const fetchCollections = async () => {
       try {
-        const { data } = await axiosReq.get('/task_collections/');
+        const { data } = await axiosReq.get("/task_collections/");
         setCollections(data);
-        console.log('Collections response:', data);
+        console.log("Collections response:", data);
       } catch (error) {
-        console.error('Error fetching collections:', error);
+        console.error("Error fetching collections:", error);
       }
-    }
-
+    };
+    // Function to fetch user tasks from the API
     const fetchTasks = async () => {
       try {
         const { data } = await axiosReq.get(`/tasks/`);
@@ -37,31 +38,33 @@ const Profile = () => {
         console.log(err);
       }
     };
-
+    // Call the fetchTasks and fetchCollections functions
     fetchTasks();
     fetchCollections();
+    // Set hasLoaded to true once the data has been loaded
     setHasLoaded(true);
   }, []);
-
+  // Count the number of completed tasks
   const completedTasksCount = Array.isArray(tasks.results)
-  ? tasks.results.filter((task) => task.is_done).length
-  : 0;
+    ? tasks.results.filter((task) => task.is_done).length
+    : 0;
 
   return (
     <>
-    <Row className="text-center">
-        <Col> <p>Image</p></Col>
+      {/* Display user profile information */}
+      <Row className="text-center">
+        <Col>
+          {" "}
+          <p>Image</p>
+        </Col>
         <h4>{userProfileName}'s Profile</h4>
         <p>Total numer tasks: {tasks.results.length}</p>
         <p>Total number of collections: {collections.results.length}</p>
         <p>Finished tasks: {completedTasksCount}</p>
         <p>Current goals:</p>
-
       </Row>
-      
-      <div><p>Test profile</p></div>
     </>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
