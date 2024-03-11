22e6f307d844d11df03c238edf787967ebb23f04
Karol S <karol96266@gmail.com>
2024-03-11 15:37:37 +0000

    Format

--- src/hooks/useClickOutsideToggle.js
+++ src/hooks/useClickOutsideToggle.js
@@ -1,11 +1,11 @@
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState, useRef } from "react";
 
 function useClickOutsideToggle() {
-    const [expanded, setExpanded] = useState(false);
+  const [expanded, setExpanded] = useState(false);
   const ref = useRef(null);
 
   // Hook checks if referenced item has been assigned to ref var current att, then if user clicked outside of referenced button, setExpand to false which closes dropdown menu.
-  // After that add the event listener and set handleClickOutside as it's callback. After that it run cleanup function. 
+  // After that add the event listener and set handleClickOutside as it's callback. After that it run cleanup function.
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (ref.current && !ref.current.contains(event.target)) {
@@ -17,7 +17,7 @@ function useClickOutsideToggle() {
       document.removeEventListener("mouseup", handleClickOutside);
     };
   }, [ref]);
-  return {expanded, setExpanded, ref};
+  return { expanded, setExpanded, ref };
 }
 
-export default useClickOutsideToggle
+export default useClickOutsideToggle;
