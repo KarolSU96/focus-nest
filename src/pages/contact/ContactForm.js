1e346ed52206f80ab9da0e9ed36c36b72e0476dc
Karol S <karol96266@gmail.com>
2024-03-11 17:24:30 +0000

    Add comments, format

--- src/pages/contact/ContactForm.js
+++ src/pages/contact/ContactForm.js
@@ -5,21 +5,24 @@ import { Link, useNavigate } from "react-router-dom";
 import btnStyles from "../../styles/Button.module.css";
 import axios from "axios";
 
-
 const ContactForm = () => {
+  // State to manage form data
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     subject: "",
     message: "",
   });
-
+  // Destructure form data properties
   const { name, email, subject, message } = formData;
 
+  // State to manage form submission errors
   const [errors, setErrors] = useState({});
 
+  // Access the navigation function from 'react-router-dom'
   const navigate = useNavigate();
 
+  // Handle form input changes
   const handleChange = (event) => {
     setFormData({
       ...formData,
@@ -27,22 +30,28 @@ const ContactForm = () => {
     });
   };
 
-  const handleCancel= () => {
-    navigate("/")
-  }
+  // Handle cancellation by navigating back to the home page
+  const handleCancel = () => {
+    navigate("/");
+  };
 
+  // Handle form submission
   const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData();
 
+    // Append form data for submission
     formData.append("name", name);
     formData.append("email", email);
     formData.append("subject", subject);
     formData.append("message", message);
     try {
+      // Submit form data to the backend
       await axios.post("contact_forms/", formData);
+      // Navigate to the home page after successful submission
       navigate("/");
     } catch (err) {
+      // Set errors state if there is an issue with form submission
       setErrors(err.response?.data);
     }
   };
@@ -52,6 +61,7 @@ const ContactForm = () => {
       <Col className="my-auto mx-auto py-2 p-md-2" md={6}>
         <Container className="text-center">
           <h1 className="text-center">contact form </h1>
+          {/* Start of Form */}
           <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="name">
               <Form.Label>Name</Form.Label>
@@ -63,12 +73,13 @@ const ContactForm = () => {
                 onChange={handleChange}
               />
             </Form.Group>
+            {/* Display name-related errors */}
             {errors?.name?.map((message, idx) => (
               <Alert variant="warning" key={idx}>
                 {message}
               </Alert>
             ))}
-
+            {/* Subject */}
             <Form.Group className="mb-3" controlId="subject">
               <Form.Label>Subject</Form.Label>
               <Form.Control
@@ -79,12 +90,14 @@ const ContactForm = () => {
                 onChange={handleChange}
               />
             </Form.Group>
+            {/* Display subject-related errors */}
             {errors?.subject?.map((message, idx) => (
               <Alert variant="warning" key={idx}>
                 {message}
               </Alert>
             ))}
 
+            {/* Email Input */}
             <Form.Group className="mb-3" controlId="email">
               <Form.Label>Your Email</Form.Label>
               <Form.Control
@@ -95,12 +108,13 @@ const ContactForm = () => {
                 onChange={handleChange}
               />
             </Form.Group>
+            {/* Display email-related errors */}
             {errors?.email?.map((message, idx) => (
               <Alert variant="warning" key={idx}>
                 {message}
               </Alert>
             ))}
-
+            {/* Message Input */}
             <Form.Group className="mb-3" controlId="message">
               <Form.Label>Message</Form.Label>
               <Form.Control
@@ -111,12 +125,13 @@ const ContactForm = () => {
                 onChange={handleChange}
               />
             </Form.Group>
+            {/* Display message-related errors */}
             {errors?.message?.map((message, idx) => (
               <Alert variant="warning" key={idx}>
                 {message}
               </Alert>
             ))}
-
+            {/* Cancel Button */}
             <Button
               variant="primary"
               className={`me-2 ${btnStyles.CancelButton}`}
@@ -124,7 +139,7 @@ const ContactForm = () => {
             >
               Cancel
             </Button>
-
+            {/* Submit Button */}
             <Button
               className={btnStyles.ConfirmButton}
               variant="primary"
@@ -134,6 +149,7 @@ const ContactForm = () => {
             </Button>
           </Form>
         </Container>
+        {/* Sign up link */}
         <Container className="mt-4 text-center">
           <Link to="/signin">
             Don't have an account?{" "}
