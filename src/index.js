d7e85f625f6b12387e07cd6ad87e3a1a3ed4f67c
Karol S <karol96266@gmail.com>
2024-03-12 14:51:14 +0000

    Switch to createRoot becauese of newer react version

--- src/index.js
+++ src/index.js
@@ -1,31 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 import { CurrentUserProvider } from "./contexts/CurrentUserContext";
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <Router>
-//       <CurrentUserProvider>
-//         <App />
-//       </CurrentUserProvider>
-//     </Router>
-//   </React.StrictMode>
-// );
-
-
-ReactDOM.render(
-  <Router>
-    <CurrentUserProvider>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <CurrentUserProvider>
         <App />
-    </CurrentUserProvider>
-  </Router>,
-  document.getElementById("root")
-)
+      </CurrentUserProvider>
+    </Router>
+  </React.StrictMode>
+);
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
