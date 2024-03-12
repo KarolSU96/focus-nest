fcff63d6be77c7d695594522853983708a920308
Karol S <karol96266@gmail.com>
2024-03-12 13:47:36 +0000

    Add comments + format doc

--- src/pages/tasks/HomeTasksPage.js
+++ src/pages/tasks/HomeTasksPage.js
@@ -9,16 +9,24 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
 
 function HomeTasksPage({ message = "" }) {
+  // State for storing tasks and loading status
   const [tasks, setTasks] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
+
+  // Get current pathname using useLocation hook
   const { pathname } = useLocation();
+
+  // State for handling search query
   const [query, setQuery] = useState("");
 
+  // useEffect hook to fetch tasks when the component mounts or pathname/query changes
   useEffect(() => {
+    // Function to fetch tasks from the server based on the search query
     const fetchTasks = async () => {
       try {
         const { data } = await axiosReq.get(`/tasks/?search=${query}`);
-        const incompleteTasks = data.results.filter((task) => !task.is_done);
+        // Filter incomplete tasks from the results
+        data.results.filter((task) => !task.is_done);
         setTasks(data);
         setHasLoaded(true);
         console.log(data);
@@ -26,17 +34,22 @@ function HomeTasksPage({ message = "" }) {
         console.log(err);
       }
     };
+    // Set loading status to false and use a (1s) timer to delay the fetch
     setHasLoaded(false);
-    const timer = setTimeout(()=> {
+    const timer = setTimeout(() => {
       fetchTasks();
-    }, 1000)
+    }, 1000);
+
+    // Cleanup function to clear the timer
     return () => {
       clearTimeout(timer);
-    }
-  }, [pathname, query]);
+    };
+  }, [pathname, query]); // Dependency array with pathname and query as dependencies
 
+  // Get the current user from the context
   const currentUser = useCurrentUser();
 
+  // Content to display when the user is not logged in
   const loggedOutContent = (
     <div className="container">
       <h1>Please log in or register ot use the page</h1>
@@ -48,40 +61,37 @@ function HomeTasksPage({ message = "" }) {
       {currentUser ? (
         <Row className="justify-content-center">
           <Col className="col-md-8 col-lg-8 mt-3">
-            <Form
-              className=""
-              onSubmit={(event) => event.preventDefault()}
-            />
-            <Form.Control 
+            <Form className="" onSubmit={(event) => event.preventDefault()} />
+            <Form.Control
               value={query}
               onChange={(event) => setQuery(event.target.value)}
               type="text"
-              placeholder="Search tasks" />
+              placeholder="Search tasks"
+            />
+            {/* Display tasks or a message if no tasks are found */}
             {hasLoaded ? (
               <>
                 {tasks.results.length ? (
                   <InfiniteScroll
-                  children={tasks.results
-                    .filter((task) => !task.is_done)  // Filter tasks where is_done is false
-                    .map((task) => (
-                      <div key={task.id} className="my-2">
-                        <Task {...task} setTasks={setTasks} showDots />
-                      </div>
-                    ))}
-                    
-                  dataLength={tasks.results.length}
-                  loader={<LoadingSpinner />}
-                  hasMore={!!tasks.next}
-                  next={() => fetchMoreData(tasks, setTasks)}
-                />
-
+                    children={tasks.results
+                      .filter((task) => !task.is_done) // Filter tasks where is_done is false
+                      .map((task) => (
+                        <div key={task.id} className="my-2">
+                          <Task {...task} setTasks={setTasks} showDots />
+                        </div>
+                      ))}
+                    dataLength={tasks.results.length}
+                    loader={<LoadingSpinner />}
+                    hasMore={!!tasks.next}
+                    next={() => fetchMoreData(tasks, setTasks)}
+                  />
                 ) : (
                   <Container>message={message}</Container>
                 )}
               </>
             ) : (
               <Container className="mt-5">
-                <LoadingSpinner  />
+                <LoadingSpinner />
               </Container>
             )}
           </Col>
