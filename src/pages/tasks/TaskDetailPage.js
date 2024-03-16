360e2bff00327c7f9341532d554577210b09d754
Karol S <karol96266@gmail.com>
2024-03-16 14:19:21 +0000

    Format doc

--- src/pages/tasks/TaskDetailPage.js
+++ src/pages/tasks/TaskDetailPage.js
@@ -1,41 +1,40 @@
-import React, { useEffect, useState } from 'react'
-import { Col, Row } from 'react-bootstrap'
-import { useParams } from 'react-router-dom';
-import { axiosReq } from '../../api/axiosDefaults';
-import Task from './Task';
+import React, { useEffect, useState } from "react";
+import { Col, Row } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
+import Task from "./Task";
 
 function TaskDetailPage() {
-    // Retrieve the 'id' parameter from the URL using useParams hook
-    const{id} = useParams();
+  // Retrieve the 'id' parameter from the URL using useParams hook
+  const { id } = useParams();
 
-    // Define state variable 'task' using useState hook, initialized with an empty object
-    const [task, setTask] = useState({results:[]});
+  // Define state variable 'task' using useState hook, initialized with an empty object
+  const [task, setTask] = useState({ results: [] });
 
-     // Define an effect to run after the component mounts or 'id' parameter changes
-    useEffect(() => {
-        const handleMount = async() => {
-            try {
-                // Make a GET request to fetch task using axiosReq 
-                const [{data: task}] = await Promise.all([
-                    axiosReq.get(`/tasks/${id}`)
-                ])
-                // Update 'task' state with the fetched task data
-                setTask({results: [task]})
-            } catch(err) {
-            }
-        }
-        // Call the handleMount function
-        handleMount();
-    }, [id])
+  // Define an effect to run after the component mounts or 'id' parameter changes
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        // Make a GET request to fetch task using axiosReq
+        const [{ data: task }] = await Promise.all([
+          axiosReq.get(`/tasks/${id}`),
+        ]);
+        // Update 'task' state with the fetched task data
+        setTask({ results: [task] });
+      } catch (err) {}
+    };
+    // Call the handleMount function
+    handleMount();
+  }, [id]);
 
   // Render the TaskDetailPage component
   return (
     <Row className="mt-5 ">
-        <Col className="col-8 mx-auto">
+      <Col className="col-8 mx-auto">
         <Task {...task.results[0]} setTask={setTask} showDots />
-        </Col>
+      </Col>
     </Row>
-  )
+  );
 }
 
-export default TaskDetailPage
+export default TaskDetailPage;
